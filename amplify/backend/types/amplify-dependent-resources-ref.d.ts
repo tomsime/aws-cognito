af824920803191722fe1b7a4ef82d0f7369b8843
Tommaso Simeone <tommaso.simeone@gmail.com>
2022-06-30 18:23:13 +0200

    🎉 Login interface configured

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awscognitoe681b26e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,141 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from 'vue'
+  import { Authenticator } from '@aws-amplify/ui-vue';
+  import '@aws-amplify/ui-vue/styles.css';
+  import './assets/amplify.css'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      Authenticator
+    },
+    setup() {
+      const configurationOptions = ref({
+        signInConfig: {
+          isSignUpDisplayed: false
+        }
+      })
+
+      return { configurationOptions }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <Authenticator
+    :form-fields="formFields"
+    :hide-sign-up="true"
+  >
+    <template v-slot:header>
+      <div style="padding: var(--amplify-space-large); text-align: center">
+        <img
+          class="amplify-image"
+          alt="Amplify logo"
+          src="https://docs.amplify.aws/assets/logo-dark.svg"
+        />
+      </div>
+    </template>
+
+    <template v-slot:sign-in-header>
+      <h3
+        class="amplify-heading"
+        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
+      >
+        Sign in to your account
+      </h3>
+    </template>
+
+    <template v-slot:sign-in-footer>
+      <div style="text-align: center">
+        <button
+          @click="toResetPassword"
+          class="amplify-button amplify-field-group__control"
+          data-fullwidth="false"
+          data-size="small"
+          data-variation="link"
+          type="button"
+          style="font-weight: normal"
+        >
+          Reset Password
+        </button>
+      </div>
+    </template>
+
+    <template v-slot:footer>
+      <div style="padding: var(--amplify-space-large); text-align: center">
+        <p class="amplify-text" style="color: var(--amplify-colors-neutral-80)">
+          © All Rights Reserved
+        </p>
+      </div>
+    </template>
+
+    <template v-slot:confirm-sign-up-header>
+      <h3
+        class="amplify-heading"
+        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
+      >
+        Enter Information:
+      </h3>
+    </template>
+
+    <template v-slot:confirm-sign-up-footer>
+      <div>Footer Information</div>
+    </template>
+
+    <template v-slot:setup-totp-header>
+      <h3
+        class="amplify-heading"
+        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
+      >
+        Enter Information:
+      </h3>
+    </template>
+
+    <template v-slot:setup-totp-footer>
+      <div>Footer Information</div>
+    </template>
+
+    <template v-slot:confirm-sign-in-header>
+      <h3
+        class="amplify-heading"
+        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
+      >
+        Enter Information:
+      </h3>
+    </template>
+
+    <template v-slot:confirm-sign-in-footer>
+      <div>Footer Information</div>
+    </template>
+
+    <template v-slot:reset-password-header>
+      <h3
+        class="amplify-heading"
+        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
+      >
+        Enter Information:
+      </h3>
+    </template>
+
+    <template v-slot:reset-password-footer>
+      <div>Footer Information</div>
+    </template>
+
+    <template v-slot:confirm-reset-password-header>
+      <h3
+        class="amplify-heading"
+        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
+      >
+        Enter Information:
+      </h3>
+    </template>
+
+    <template v-slot:confirm-reset-password-footer>
+      <div>Footer Information</div>
+    </template>
+
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </Authenticator>
+</template>
